2750cf9833e5f4f0d59eaed739b3bb832973bc67
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-09-30 12:11:04 -0400

    added bootstrap columns and rows

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -1,17 +1,20 @@
 var tasks = {};
-
+// $("#my-id").text("myNewText")
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+  // $("li") $(".list-group-item") $("#")
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
   var taskP = $("<p>")
+  // m-1 places a margin (bootstrap)
     .addClass("m-1")
     .text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
 
 
   // append to ul list on the page
